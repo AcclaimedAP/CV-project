adba2e64eaa0af9141ce1249c5f56963c7f691aa
AcclaimedAP <alexander.w.pressfelt@gmail.com>
2022-11-01 14:51:01 +0100

    fixed menu scrolling bs

--- js/script.js
+++ js/script.js
@@ -1,13 +1,12 @@
-//Gets all links in menu
-const hreflink = document.querySelectorAll('.menu-link');
 
+const hreflink = document.querySelectorAll('.menu-link');
+const myfield = document.getElementById('menu-toggle');
 //Foreach loops through all of them
 hreflink.forEach(menulink => {
     //Adds on click eventlistener to them
     menulink.addEventListener('click', function handleClick(event) {
         //close menu by unchecking box
         myfield.checked = false;
-
 
     });
 });
